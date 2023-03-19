13dcced14beb1e820d1be234cb53023a77e36a6e
Виктор <lenkov.victor@yandex.ru>
2023-03-19 23:01:27 +0300

    11

--- main.py
+++ main.py
@@ -1,95 +1,26 @@
 import re
+import random
 #Функции
 ###################################################
 def TaskSelection():
     print("Выберите номер задачи:\n"
-          "1 - Проверка пароля.\n"
-          "2 - Тип места в плацкартном вагоне.\n"
-          "3 - Проверка високостности года.\n"
-          "4 - Смешивание цветов.\n"
-          "5 - Соединение слов в строку.")
+          "1 - Соединение слов в строку.\n"
+          "2 - Соединение слов в строку (слово stop).\n"
+          "3 - Редкие слова.\n"
+          "4 - Математика для детей.")
     number = input()
     match number:
         case "1":
-            PasswordVerification()
-        case "2":
-            TypeOfPlace()
-        case "3":
-            CheckingTheLeapYear()
-        case "4":
-            MixingColors()
-        case "5":
             ConnectingWordsToAString()
+        case "2":
+            ConnectingWordsToAStringStop()
+        case "3":
+            RareWords()
+        case "4":
+            MathForKids()
         case _:
             print("Введен неправильный номер.")
             TaskSelection()
-
-###################################################
-def PasswordVerification():
-    print('Введите пароль.')
-    FirstPas = input()
-    pattern = re.fullmatch(r"[A-Za-z0-9\-_\\/\%!\?=()\+\$\.\*\[\]\{\}]{8,}", FirstPas)
-    if pattern == None:
-        print('Пароль не принят! (Пароль либо слишком короткий или содержит запрещенные символы.)')
-        PasswordVerification()
-    print('Подтвердите пароль.')
-    SecondPas = input()
-    if FirstPas == SecondPas:
-        print('Пароль принят.')
-        exit()
-    else:
-        print('Пароль не принят! (Пароли не совпадают.)')
-        PasswordVerification()
-
-###################################################
-def TypeOfPlace():
-    Number = input('Введите номер вашего места в плацкарте: ')
-    pattern = re.fullmatch(r"[0-9]{1,2}", Number)
-    if pattern and int(Number) > 0 and int(Number) <= 54:
-        Number = int(Number)
-        if Number < 37 and Number % 2:
-            print('Ваше место - в купе внизу.')
-        elif Number < 37 and Number % 2 == 0:
-            print('Ваше место - в купе наверху.')
-        elif Number % 2:
-            print('Ваше место - боковое внизу.')
-        else:
-            print('Ваше место - боковое наверху.')
-    else:
-        print('Введен неправильный номер!')
-        TypeOfPlace()
-
-###################################################
-def CheckingTheLeapYear():
-    Number = input('Введите год: ')
-    pattern = re.fullmatch(r"[0-9]{1,4}", Number)
-    if pattern:
-        Number = int(Number)
-        if ((Number % 4 == 0) and not (Number % 100 == 0)) or (Number % 400 == 0):
-            print('Год', Number, ' - високосный.')
-        else:
-            print('Год', Number, ' - невисокосный.')
-    else:
-        print('Введен неправильный год!')
-        CheckingTheLeapYear()
-
-###################################################
-def MixingColors():
-    print('Введите первый цвет.')
-    FirstColor = input()
-    print('Введите второй цвет.')
-    SecondColor = input()
-    FColor = FirstColor.lower()
-    SColor = SecondColor.lower()
-    if (FColor == 'красный' and SColor == 'синий') or (FColor == 'синий' and SColor == 'красный'):
-        print('При смешении красного и синего получится фиолетовый')
-    elif (FColor == 'красный' and SColor == 'желтый') or (FColor == 'желтый' and SColor == 'красный'):
-        print('При смешении красного и желтого получится оранжевый')
-    elif (FColor == 'синий' and SColor == 'желтый') or (FColor == 'желтый' and SColor == 'синий'):
-        print('При смешении синего и желтого получится зеленый')
-    else:
-        print('Введен неправильный цвет!')
-        MixingColors()
 
 ###################################################
 def ConnectingWordsToAString():
@@ -105,6 +36,68 @@ def ConnectingWordsToAString():
     str = ''
     while counter <= Number:
         print('Введите ', counter, ' слово.')
+        Word = input()
+        pattern2 = re.fullmatch(r"[A-Za-zА-Яа-яЁё\-]{1,}", Word)
+        if pattern2:
+            counter = counter + 1
+            str = str + Word + ' '
+    print(str.rstrip())
+###################################################
+
+def ConnectingWordsToAStringStop():
+    print('Введите количество слов.')
+    Number = input()
+    pattern1 = re.fullmatch(r"[0-9]{1,}", Number)
+    if pattern1:
+        Number = int(Number)
+    else:
+        ConnectingWordsToAStringStop()
+
+    counter = 1
+    str = ''
+    while counter <= Number:
+        print('Введите ', counter, ' слово.')
+        Word = input()
+        if Word == 'stop':
+            break
+        pattern2 = re.fullmatch(r"[A-Za-zА-Яа-яЁё\-]{1,}", Word)
+        if pattern2:
+            counter = counter + 1
+            str = str + Word + ' '
+    print(str.rstrip())
+###################################################
+
+def RareWords():
+    #while counter <= Number:
+        print('Введите слово.')
+        Word = input()
+        pattern = re.fullmatch(r"[A-Za-zА-Яа-яЁё\-]{1,}", Word)
+        if pattern:
+            symbol = 'ф'
+            if symbol in Word:
+                print("Ого! Это редкое слово!")
+            else:
+                print("Эх, это не очень редкое слово...")
+        else:
+            RareWords()
+
+###################################################
+
+def MathForKids():
+    counter = 0
+    while counter <= 3:
+        num1 = random.randint(1, 10)
+        num2 = random.randint(1, 10)
+        #str2 = str(num1) + ' + ' + str(num2) + ' = '
+        str1 = str(55)
+        str2 = str(44)
+        print(num1)
+        print(num2)
+        print(str1)
+        print(str2)
+        Number = input(str2)
+        if Number.isdigit():
+            print('Введите ', counter, ' слово.')
         Word = input()
         pattern2 = re.fullmatch(r"[A-Za-zА-Яа-яЁё\-]{1,}", Word)
         if pattern2:
